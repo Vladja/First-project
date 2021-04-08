7a825df100c798e509b88c839523a5314de57ebb
Vladislav <vladja.developer@gmail.com>
2021-04-08 17:10:13 +0300

    Доделал практическое занятие №2( циклы, условия)

--- JS/script.js
+++ JS/script.js
@@ -8,12 +8,27 @@ const PersonalMovieDB = {
    privat: false
 };
 
-const a = prompt("Какой фильм вы хотите оценить?", ""),
-      b = prompt("Какую оценку вы хотите дать этому фильму?", ""),
-      c = prompt("Какой фильм вы хотите оценить?", ""),
-      d = prompt("Какую оценку вы хотите дать этому фильму?", "");
+for (let i = 1; i <= 2; i++){
+   const a = prompt("Какой фильм вы хотите оценить?", ""),
+         b = prompt("Какую оценку вы хотите дать этому фильму?", "");
 
-PersonalMovieDB.movies[a] = b;
-PersonalMovieDB.movies[c] = d;
+   if (a != null && b != null && a != '' && b != '' && a.length < 50){
+      PersonalMovieDB.movies[a] = b;
+      console.log("done!");
+   } else {
+      console.log("Error");
+      i--;
+   }     
+}
+
+if (PersonalMovieDB.count < 10) {
+   alert("Просмотрено слишком мало фильмов");
+} else if (PersonalMovieDB.count >= 10 && PersonalMovieDB.count < 30) {
+   alert("Вы классический зритель");
+} else if(PersonalMovieDB.count >= 30){
+   alert("Да вы прям киноман!");
+} else{
+   alert("Error");
+}
 
 console.log(PersonalMovieDB);
